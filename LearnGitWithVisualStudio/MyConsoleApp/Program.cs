b4f50a5e7bca8abe2b24f5be2ff11932fd87426b
Corrado Cavalli <corrado@outlook.com>
2018-05-08 12:42:42 +0200

    Added PR1

--- LearnGitWithVisualStudio/MyConsoleApp/Program.cs
+++ LearnGitWithVisualStudio/MyConsoleApp/Program.cs
@@ -11,6 +11,7 @@ namespace MyConsoleApp
         static void Main(string[] args)
         {
             if (args == null) throw new ArgumentNullException(nameof(args));
+            //This is PR1
         }
 
         public int P1 { get; set; }
